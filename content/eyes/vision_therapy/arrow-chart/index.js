4a821a4da7a0c04aa31ea82104ec7753f011d1a9
Graham Scanlon <notes@ascallion.com>
2026-02-07 11:08:17 -0600

    wip: arrow chart arrows

--- content/eyes/vision_therapy/arrow-chart/index.js
+++ content/eyes/vision_therapy/arrow-chart/index.js
@@ -5,6 +5,7 @@ class Consts {
 const arrows = ["↑", "↓", "←", "→"];
 const chart = document.getElementById("arrow-chart");
 const regenerate = document.getElementById("regenerate");
+const arrowSelect = document.getElementById("arrow-select");
 const rowsColsRange = document.getElementById("row-col-range");
 const fontSizeRange = document.getElementById("font-size-select");
 const letterGapRange = document.getElementById("letter-gap-select");
@@ -38,6 +39,10 @@ setLetterGapRange();
 const setRowsCols = () => Consts.ROWS_COLS = parseInt(rowsColsRange.value);
 rowsColsRange.addEventListener('input', setRowsCols);
 setRowsCols();
+
+const setArrow = () => console.debug(arrowSelect.value);
+arrowSelect.addEventListener('change', setArrow)
+setArrow();
 
 redGreenCheck.addEventListener('change', generateArrowChart);
 rowsColsRange.addEventListener('change', generateArrowChart);
--- content/eyes/vision_therapy/arrow-chart/index.md
+++ content/eyes/vision_therapy/arrow-chart/index.md
@@ -18,6 +18,7 @@ options = [
   { name = "Red Color", id = "red-color", type = "color", value = "#FF0000" },
   { name = "Green Color", id = "green-color", type = "color", value = "#008000" },
   { name = "Red/Green Arrows", id = "red-green-checkbox", type = "checkbox" },
+  { name = "Type of Arrow (todo)", id = "arrow-select", type = "select", options = ["arrow", "fat-arrow"] },
   { name = "Font Size", id = "font-size-select", type = "range", min = 10, max = 60, value = 40 },
   { name = "Letter Gap", id = "letter-gap-select", type = "range", min = 0, max = 25, value = 10 },
   { name = "Rows & Columns", id = "row-col-range", type = "range", min = 2, max = 10, value = 10 },
